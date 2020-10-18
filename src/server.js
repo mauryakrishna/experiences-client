0f79f01c5a6b4768827025ced8c3e779a4eb7081
mauryakrishna <mauryakrishna1@gmail.com>
2020-10-18 20:03:36 +0530

    remove the data/models usage from server.js and remove sqlite3 from yarn lock

--- src/server.js
+++ src/server.js
@@ -31,7 +31,6 @@ import errorPageStyle from './routes/error/ErrorPage.css';
 import passport from './auth/passport';
 import authrouter from './auth/routes';
 import router from './router';
-import models from './data/models';
 
 // import assets from './asset-manifest.json'; // eslint-disable-line import/no-unresolved
 import chunks from './chunk-manifest.json'; // eslint-disable-line import/no-unresolved
@@ -199,12 +198,9 @@ app.use((err, req, res, next) => {
 //
 // Launch the server
 // -----------------------------------------------------------------------------
-const promise = models.sync().catch(err => console.error(err.stack));
 if (!module.hot) {
-  promise.then(() => {
-    app.listen(config.port, () => {
-      console.info(`The server is running at http://localhost:${config.port}/`);
-    });
+  app.listen(config.port, () => {
+    console.info(`The server is running at http://localhost:${config.port}/`);
   });
 }
 
