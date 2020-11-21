176ce6a85ac8f8f8fcb64054ffc95198ead3b055
krishna maurya <mauryakrishna1@gmail.com>
2020-11-21 17:22:45 +0530

    semicolon

--- src/routes/editor/MobileNotSupported.js
+++ src/routes/editor/MobileNotSupported.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'React';
 import { Box } from '@chakra-ui/core';
 import { InfoMessage } from '../../components/UIElements';
 
@@ -8,7 +8,7 @@ export default () => {
       <InfoMessage>
         Currently, editor is not supported on mobile devices.
         You can use the desktop browser to write you experiences.
-        </InfoMessage>
+      </InfoMessage>
     </Box>
   )
 }
