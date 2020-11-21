ed8c7db5aede1d3b32c19fe75d737a4847f2b0d9
krishna maurya <mauryakrishna1@gmail.com>
2020-11-21 16:29:53 +0530

    mobile not supported for editor message

---
+++ src/routes/editor/MobileNotSupported.js
@@ -0,0 +1,14 @@
+import React from 'React'
+import { Box } from '@chakra-ui/core';
+import { InfoMessage } from '../../components/UIElements';
+
+export default () => {
+  return (
+    <Box>
+      <InfoMessage>
+        Currently, editor is not supported on mobile devices.
+        You can use the desktop browser to write you experiences.
+        </InfoMessage>
+    </Box>
+  )
+}
--- src/routes/editor/index.js
+++ src/routes/editor/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import Editor from './Editor';
+import DetectMobile from '../../detectmobilebrowser';
+import MobileNotSupported from './MobileNotSupported';
 
 const title = 'Write an experience';
 
 function action(context, params) {
+  const IsMobile = DetectMobile();
   return {
     chunks: ['edit-editor', 'write-editor'],
     title,
     component: (
       <Layout pathname={context.pathname}>
-        <Editor slugkey={params.slugkey} />
+        {
+          IsMobile ? <MobileNotSupported /> :
+            <Editor slugkey={params.slugkey} />
+        }
+
       </Layout>
     ),
   };
