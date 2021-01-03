25b2b248da8f9e00e6cbff0528b4868d0854a605
krishna maurya <mauryakrishna1@gmail.com>
2021-01-03 15:32:15 +0530

    meta description modified

--- src/components/Html.js
+++ src/components/Html.js
@@ -30,7 +30,7 @@ export default function Html({
         <title>{title}</title>
         <meta name="google-site-verification" content="UZYrsB1BXHH_HQD8QpPlyJJZZYUCVPIOrkO4XLXhqII" />
         <meta name="description" 
-          content="Write your experiences, life learnings, lessons of day to day life, blog, share feeling, keep a draft, publish and many more." />
+          content="Write your experiences, share you life's experiences, lessons of day to day life, help others share your life's experiences, experiences of life." />
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {scripts.map(script => (
