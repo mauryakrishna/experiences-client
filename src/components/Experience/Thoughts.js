a73d4cfc2dc3ae539b07708dfc28fa8bc134ce6a
krishna maurya <mauryakrishna1@gmail.com>
2021-01-10 22:11:23 +0530

    WIP ui for thoughts

---
+++ src/components/Experience/Thoughts.js
@@ -0,0 +1,6 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { createEditor } from 'slate';
+import { Slate } from 'slate-react';
+import { EditablePlugins, pipe } from '@udecode/slate-plugins';
+
