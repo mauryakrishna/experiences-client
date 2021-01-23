54d36e8f26f56581d76708be88daed3ad05773f0
krishna maurya <mauryakrishna1@gmail.com>
2021-01-23 19:44:52 +0530

    loadable removed, used inline import instead

--- src/utils/IsDifferent.js
+++ src/utils/IsDifferent.js
@@ -1,7 +1,6 @@
-import loadable from '@loadable/component';
 
 /* Compare object equality returns true if different */
-export default (value1, value2) => {
-  const isEqual = loadable(()=> import('lodash/isEqual'))
+export default async (value1, value2) => {
+  const { default: isEqual } = await import('lodash/isEqual');
   return !isEqual(value1, value2);
 };
