3a1c075d4c75e7e599ed0ff710b9b534f7207de3
krishna maurya <mauryakrishna1@gmail.com>
2021-01-26 21:13:46 +0530

    added authorization token in header

--- src/gqloverhttp.js
+++ src/gqloverhttp.js
@@ -1,7 +1,14 @@
+import localStorage from "local-storage";
+
 export default async ({ variables, query }) => {
+  const token = localStorage.get('token');
   const { data } = await fetch(`${window.location.origin}/gql`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json',
+      'Accept-Encoding': 'gzip,deflate',
+      ...(token && { authorization: token }),
+     },
     body: JSON.stringify({
       query,
       variables,
