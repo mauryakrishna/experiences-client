f949865cf3007c9b1bc4b90d32fb9100a5b54ba6
krishna maurya <mauryakrishna1@gmail.com>
2021-03-04 07:54:53 +0530

    display pencel icon only if not editor route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import Link from '../Link';
 const Authenticate = loadable(()=> import('./Authenticate'))
 
 export default function Header() {
+  const { pathname } = history && history.location;
   const takeToEditor = () => {
     history.push("/writeanexperience");
   }
@@ -29,18 +30,24 @@ export default function Header() {
             </Text>
         </Link>
       </Flex>
-      <Flex marginTop="3px" marginRight="auto" onClick={takeToEditor} cursor="pointer">
-        <svg 
-          stroke="blanchedalmond"
-          fill="darkgray"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24" 
-          aria-hidden="true"
-        >
-          <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
-        </svg>
-      </Flex>
+      {
+        pathname && (pathname !== "/writeanexperience") &&
+        (
+          <Flex marginTop="3px" marginRight="auto" onClick={takeToEditor} cursor="pointer">
+            <svg 
+              stroke="blanchedalmond"
+              fill="darkgray"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24" 
+              aria-hidden="true"
+            >
+              <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+            </svg>
+          </Flex>
+        )
+      }
+      
       <Box>
         <Authenticate />
       </Box>
