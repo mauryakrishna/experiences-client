bbf6744eb041005552e61b066983e248dbb1b513
krishna maurya <mauryakrishna1@gmail.com>
2021-04-08 20:24:24 +0530

    meta tags for twitter added

--- src/seo/index.js
+++ src/seo/index.js
@@ -8,10 +8,16 @@ export default ({ title, description, canonical })=> {
       <meta name="title" content={title} />
       <meta name="description" content={description} />
       <link rel="canonical" href={canonical} />
-
+      
+      {/* og (Open Graph potocol) Facebook, LinkedIn */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={canonical} />
+      
+      {/* twitter specific tags */}
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   )
 }
