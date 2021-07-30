57830532e4c4223afd428760da4f61a86ddd7e12
Krishna maurya <kmaurya@intraedge.com>
2021-07-30 21:00:22 +0530

    commented code deleted

--- src/components/Editor/TitleCombobox.js
+++ src/components/Editor/TitleCombobox.js
@@ -1,46 +1,13 @@
-import React, { useState, useRef } from 'react'
-import {render} from 'react-dom'
-import {useCombobox} from 'downshift'
-import {items, menuStyles, comboboxStyles} from './shared'
+import React from 'react'
 import {
   ComboboxItem,
   ComboboxRoot,
 } from '../AutoSuggestions/components/TagCombobox.styles'
 
 function DropdownCombobox({ menuProps, isOpen, highlightedIndex, comboboxRef, inputItems, getItemProps }) {
-  // const [inputItems, setInputItems] = useState(items)
-  // const {
-  //   isOpen,
-  //   getToggleButtonProps,
-  //   getLabelProps,
-  //   getMenuProps,
-  //   getInputProps,
-  //   getComboboxProps,
-  //   highlightedIndex,
-  //   getItemProps,
-  // } = useCombobox({
-  //   items: inputItems,
-  //   onInputValueChange: ({inputValue}) => {
-  //     setInputItems(
-  //       items.filter((item) =>
-  //         item.toLowerCase().startsWith(inputValue.toLowerCase()),
-  //       ),
-  //     )
-  //   },
-  // })
+  
   return (
     <div>
-      {/* <label {...getLabelProps()}>Choose an element:</label>
-      <div style={comboboxStyles} {...getComboboxProps()}>
-        <input {...getInputProps()} />
-        <button
-          type="button"
-          {...getToggleButtonProps()}
-          aria-label="toggle menu"
-        >
-          &#8595;
-        </button>
-      </div> */}
       <ComboboxRoot {...menuProps} ref={comboboxRef} isOpen={isOpen}>
         {isOpen &&
           inputItems.map((item, index) => (
