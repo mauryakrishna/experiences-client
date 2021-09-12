a71206f23cf1e609e03da3be100533b76c961dfa
mauryakrishna <mauryakrishna1@gmail.com>
2021-09-12 13:37:53 +0530

    handle crash when backspacing on non english character, keeping typed in translation in cache

--- src/services/autosuggestions.js
+++ src/services/autosuggestions.js
@@ -1,5 +1,37 @@
+import find from "lodash/find"
+
+const cache = [];
+const getFromCache = (inString) => {
+    return find(cache, { inString })
+}
+
+const updateCache = (json) => {
+    if(!getFromCache(json.inString)) {
+        cache.push(json)
+    }
+}
+
+const getNonEnglish = (itrans) => {
+    return find(cache, { itrans })
+}
+
+const isEnglishCharacter = new RegExp("[a-zA-Z0-9]")
+
 export default (inString) => {
-    console.log("inString", inString)
+    if(!isEnglishCharacter.test(inString)) {
+        return Promise.resolve(getNonEnglish(inString))
+    }
+
+    const found = getFromCache(inString)
+    if(found) {
+        return Promise.resolve(found)
+    }
+    else {
+        return getFromService(inString)
+    }
+}
+
+const getFromService = (inString) => {
     return fetch(`${window.location.origin}/processWordJSON?inString=${inString}&lang=hindi`, {
         method: 'GET',
         headers: { 
@@ -9,8 +41,14 @@ export default (inString) => {
     }).then(resp => {
         return resp.json()
     })
+    .then((json) => {
+        if(json) {
+            updateCache(json)
+        }
+        return json
+    })
     .catch((error) => {
         console.log("Error in autosuggestion", error)
-        return { tword: [{ options: []}]}
+        return null
     })
 }
