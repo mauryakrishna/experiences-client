e9d0ba7f1ab6cbe23e292f5ab2b1162ad53568bd
mauryakrishna <mauryakrishna1@gmail.com>
2021-09-18 00:44:33 +0530

    get options and display

--- src/components/AutoSuggestions/hooks/useTagOnChange.js
+++ src/components/AutoSuggestions/hooks/useTagOnChange.js
@@ -3,12 +3,13 @@ import { MentionNodeData } from '@udecode/slate-plugins'
 import shallow from 'zustand/shallow'
 import { useComboboxOnChange } from '../../ComboBox/hooks/useComboboxOnChange'
 import { ComboboxKey, useComboboxStore } from '../../ComboBox/useComboboxStore'
+import autoSuggestion from "../../../services/autosuggestions"
 
 export const useTagOnChange = (editor, data) => {
   const comboboxOnChange = useComboboxOnChange({
     editor,
     key: ComboboxKey.TAG,
-    trigger: `[A-za-z0-9]`
+    trigger: `[A-Za-z0-9]`
   })
   const { maxSuggestions, setItems, setTargetRange } = useComboboxStore(
     // eslint-disable-next-line @typescript-eslint/no-shadow
@@ -25,19 +26,19 @@ export const useTagOnChange = (editor, data) => {
     if (!res) return false
 
     const { search } = res
-
-    const items = data
-      .filter((c) => c.name.toLowerCase().includes(search.toLowerCase()))
-      .slice(0, maxSuggestions)
-      .map((item) => ({
-        key: item.value,
-        text: item.name,
-      }))
-
-    setItems(items)
-    // this is when, user continues to type and no match from the combobox then set the range as null
-    // so as to close the combobox
-    items.length === 0 && setTargetRange(null)
+    const items = []
+    autoSuggestion(search)
+      .then((resp)=> {
+        if(resp) {
+          setItems(resp.twords[0].options)
+        }
+        else {
+          // this is when, user continues to type and no match from the combobox then set the range as null
+          // so as to close the combobox
+          items.length === 0 && setTargetRange(null)
+          setItems([])
+        }
+      })
 
     return true
   }, [comboboxOnChange, data, maxSuggestions, setItems])
